6450c811734c848c44d692f8d8d12fcdfb368812
Laurens Leeuwis <laurens.leeuwis@gmail.com>
2015-06-24 09:30:08 +0200

    add ngRoute from CDN

--- src/main/webapp/scripts/app.js
+++ src/main/webapp/scripts/app.js
@@ -1,4 +1,23 @@
 'use strict';
 
 angular
-  .module('testApp', []);
+    .module('testApp', ['ngRoute'])
+    .config(
+        function($routeProvider) {
+            $routeProvider.
+                when('/login', {
+                    templateUrl: 'views/login.html',
+                    controller: 'LoginCtrl'
+                }).
+                when('/main', {
+                    templateUrl: 'views/main.html',
+                    controller: 'MainCtrl'
+                }).
+                when('/secret', {
+                    templateUrl: 'views/secret.html',
+                    controller: 'SecretCtrl'
+                }).
+                otherwise({
+                    redirectTo: '/main'
+                });
+        });
